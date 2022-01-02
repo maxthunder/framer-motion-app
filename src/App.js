e81bc0ee54d79e22060579a9f50fc92fabd54019
maxthunder <maxwell.stark@gmail.com>
2022-01-01 22:07:47 -0700

    Adds react-bootstrap. Adds divider component.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from "react";
 import "./App.css";
 import Rotate from "./components/Rotate";
 import FramerMotion from "./components/FramerMotion"
+import Alert from 'react-bootstrap/Alert'
 
 export default function App() {
   return (
     <div>
       <Rotate/>
+      <Divider />
       <FramerMotion/>
+      <Divider />
     </div>
   );
+}
+
+const Divider = () => {
+  return <Alert variant="dark"></Alert>
 }
