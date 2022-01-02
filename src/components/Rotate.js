c2f10603c68b56b7826e8a67c9bb72cdf0c706e0
maxthunder <maxwell.stark@gmail.com>
2022-01-01 22:50:41 -0700

    adds defaultState to initial/exit to facilitate fade in/out during mount/unmount

--- src/components/Rotate.js
+++ src/components/Rotate.js
@@ -5,16 +5,22 @@ import ".././App.css"
 const Rotate = () => {
   const [isActive, setIsActive] = useState(false);
 
+  const defaultState = {// for mount/unmount animation (fade in/out)
+    opacity: 0
+  }
+
   const variant = {
     active: {
       rotate: 360,
       x: 850,
-      background: 'lightcoral'
+      background: 'lightcoral',
+      opacity: 1
     },
     inactive: {
       rotate: 0,
       x: 0,
-      background: '#fff'
+      background: '#fff',
+      opacity: 1
     }
   };
 
@@ -23,6 +29,8 @@ const Rotate = () => {
       className="block"
       onClick={() => setIsActive(!isActive)}
       variants={variant}
+      initial={defaultState}
+      exit={defaultState}
       animate={isActive ? variant.active : variant.inactive}
       transition={{
         duration: 0.33 // seconds
